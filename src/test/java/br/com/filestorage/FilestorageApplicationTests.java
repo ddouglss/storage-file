550853082f4e1864c8134274bed624cb90a1c286
ddouglss <ddouglss1999@gmail.com>
2025-09-08 22:18:54 -0300

    primeiro commit

---
+++ src/test/java/br/com/filestorage/FilestorageApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.filestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilestorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
